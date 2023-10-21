b1681d6a85cdab1fe05f666f3d6cd38b10757651
Tang <a_tang_ya@163.com>
2023-10-21 18:07:39 +0800

    feat: 删除 mock 请求使用真实请求

--- src/utils/http/axios/Axios.ts
+++ src/utils/http/axios/Axios.ts
@@ -211,6 +211,7 @@ export class VAxios {
       this.axiosInstance
         .request<any, AxiosResponse<Result>>(conf)
         .then((res: AxiosResponse<Result>) => {
+          console.log(123);
           if (transformRequestHook && isFunction(transformRequestHook)) {
             try {
               const ret = transformRequestHook(res, opt);
