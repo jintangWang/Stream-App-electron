a6df280858afaf6e558e9f5ba5a1ccae147427ea
Tang <a_tang_ya@163.com>
2023-10-23 23:03:34 +0800

    feat: 个人中心添加用户头像

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -3,9 +3,17 @@
     <div class="py-8 px-8 bg-white flex flex-col justify-center">
       <div class="base-info">
         <div class="row">
+          <span class="label">用户头像</span>
+          <img :src="baseUrl + userinfo.avatar" alt="" class="w-60px mx-auto" />
+        </div>
+        <div class="row">
           <span class="label">用户名</span>
           <span class="value">{{ userinfo.username }}</span>
         </div>
+        <!-- <div class="row">
+          <span class="label">性别</span>
+          <span class="value">{{ userinfo.gender === 'MALE' ? '男' : '女' }}</span>
+        </div> -->
       </div>
 
       <BasicForm @register="register" />
@@ -21,12 +29,14 @@
   import { PageWrapper } from '/@/components/Page';
   import { BasicForm, useForm } from '/@/components/Form';
   import { useUserStore } from '/@/store/modules/user';
+  import { getAppEnvConfig } from '/@/utils/env';
 
   import { formSchema } from './profile.data';
   export default defineComponent({
     name: 'Profile',
     components: { BasicForm, PageWrapper },
     setup() {
+      const { VITE_GLOB_API_URL } = getAppEnvConfig();
       const [register, { validate, resetFields }] = useForm({
         size: 'large',
         labelAlign: 'left',
@@ -52,7 +62,7 @@
         } catch (error) {}
       }
 
-      return { register, resetFields, handleSubmit, userinfo };
+      return { register, resetFields, handleSubmit, userinfo, baseUrl: VITE_GLOB_API_URL };
     },
   });
 </script>
