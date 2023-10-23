04999931177681dfb88f8e9ecc964282e77d250c
Tang <a_tang_ya@163.com>
2023-10-23 17:00:25 +0800

    feat: 个人中心页面修改

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,6 +1,13 @@
 <template>
   <PageWrapper title="个人中心" content="修改密码成功后会自动退出当前登录！">
-    <div class="py-8 bg-white flex flex-col justify-center items-center">
+    <div class="py-8 px-8 bg-white flex flex-col justify-center">
+      <div class="base-info">
+        <div class="row">
+          <span class="label">用户名</span>
+          <span class="value">{{ userinfo.username }}</span>
+        </div>
+      </div>
+
       <BasicForm @register="register" />
       <div class="flex justify-center">
         <a-button @click="resetFields"> 重置 </a-button>
@@ -10,9 +17,10 @@
   </PageWrapper>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent, computed } from 'vue';
   import { PageWrapper } from '/@/components/Page';
   import { BasicForm, useForm } from '/@/components/Form';
+  import { useUserStore } from '/@/store/modules/user';
 
   import { formSchema } from './profile.data';
   export default defineComponent({
@@ -21,10 +29,16 @@
     setup() {
       const [register, { validate, resetFields }] = useForm({
         size: 'large',
+        labelAlign: 'left',
         labelWidth: 100,
         showActionButtonGroup: false,
         schemas: formSchema,
+        labelCol: { span: 6 },
+        wrapperCol: { span: 16 },
       });
+
+      const userStore = useUserStore();
+      const userinfo = computed(() => userStore.getUserInfo);
 
       async function handleSubmit() {
         try {
@@ -38,7 +52,22 @@
         } catch (error) {}
       }
 
-      return { register, resetFields, handleSubmit };
+      return { register, resetFields, handleSubmit, userinfo };
     },
   });
 </script>
+<style lang="less" scoped>
+  .base-info {
+    align-self: flex-start;
+  }
+  .row {
+    height: 40px;
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+    .label {
+      width: 100px;
+      text-align: left;
+    }
+  }
+</style>
